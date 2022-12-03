04d283ce1d52a67995f9a5c132917405e026eb84
MBrandt <vashathearchivist6789@gmail.com>
2022-12-02 21:41:53 -0500

    working?

---
+++ index.js
@@ -0,0 +1,173 @@
+const inquirer = require("inquirer");
+const fs = require ("fs");
+
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const generateHTML = require('./src/generateHTML');
+
+//empty array for team profiles
+const teamArr = [];
+
+function init(){
+    managerProfile();
+}
+
+function managerProfile () {
+   inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team managers name?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log ('Please enter the managers name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team managers id?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team managers email address?'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the team managers office number?'
+        }
+    ])
+    .then(({name, id, email, officeNumber}) => {
+        this.Manager = new Manager(name, id, email, officeNumber);
+        teamArr.push(this.Manager)
+        // console.log(manager);
+        addEmployee();
+
+    })
+
+};
+
+
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Please choose an employees role?',
+            choices: ['Engineer', 'Intern', 'Done']
+        }
+])
+.then (({role}) => {
+    if (role === 'Engineer'){
+        engineerProfile();
+    } else if (role === 'Intern') {
+        internProfile();
+    }else{
+        const fileData = generateHTML(teamArr);
+        writeFile(fileData, teamArr);
+    }
+})
+
+};
+
+function engineerProfile() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the engineers name?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log ('Please enter the managers name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the engineers id?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineers email address?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineers GitHub username?'
+        }
+    ])
+    .then(({name, id, email, github}) => {
+        this.Engineer = new Engineer(name, id, email, github);
+        teamArr.push(this.Engineer)
+        addEmployee();
+
+    })
+};
+
+function internProfile (){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the interns name?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log ('Please enter the managers name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the interns id?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the interns email address?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school does the intern go to?'
+        }
+    ])
+    .then(({name, id, email, school}) => {
+        this.Intern = new Intern(name, id, email, school);
+        teamArr.push(this.Intern)
+        addEmployee();
+
+    })
+};
+
+function writeFile(filedata, teamArr){
+    fs.writeFile('./dist/index.html', filedata, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }else {
+            console.log("Team profile has been created")
+        }
+    })
+};
+
+
+
+init();
---
+++ src/generateHTML.js
@@ -0,0 +1,123 @@
+const generateHTML = teamProfiles => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/164afc97fd.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+   
+        <div class="container-fluid bg-info">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team Profiles</h1>
+                </div>
+            </div>
+        </div>
+   
+        <div class="container col-12 p-3">
+            <div class="row d-flex justify-content-center">
+            
+                 ${teamProfiles}
+            </div>
+        
+        </div>
+    </body>
+    
+</html>
+`
+}
+
+const generateTeamPage = team => {
+    const teamCards = [];
+    for (let i = 0; team.length > i; i++) {
+        const teamProf = team[i]
+        const teamRole = teamProf.getRole();
+console.log("role", teamRole)
+        if (teamRole === 'Manager') {
+            const manager = managerProfile(teamProf);
+            teamCards.push(manager);
+        }
+
+        if (teamRole === 'Engineer') {
+            const engineer = engineerProfile(teamProf);
+            teamCards.push(engineer);
+        }
+
+        if (teamRole === 'Intern') {
+            const intern = internProfile(teamProf);
+            teamCards.push(intern);
+        }
+    }
+    const teamProfiles = teamCards.join('');
+    return generateHTML(teamProfiles);
+
+}
+
+const managerProfile = manager => {
+    return `
+    <div class="card col-3 m-3">
+        <div class="card-header">
+            <h2>${manager.name}</h2>
+            <h3><i class="fa-solid fa-mug-hot mr-2"></i>Manager</h3>
+        </div>
+        <div class="card-body bg-secondary">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <a
+                    href="mailto:${manager.email}">${manager.email}</a></li>
+                <li class="list-group-item">Office number: ${manager.officeNumber} </li>
+            </ul>
+        </div>
+    </div>
+    `
+};
+
+
+const engineerProfile = engineer => {
+    return `
+    <div class="card col-3 m-3">
+        <div class="card-header">
+            <h2>${engineer.name}</h2>
+            <h3><i class="fa-solid fa-glasses"></i>Engineer</h3>
+        </div>
+        <div class="card-body bg-secondary">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: <a
+                    href="mailto:${engineer.email}">${engineer.email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    `
+};
+
+
+const internProfile = intern => {
+    return `
+    <div class="card col-3 m-3">
+         <div class="card-header">
+            <h2 class="card-title">${intern.name}</h2>
+            <h3 class="card-title"><i class="fa-solid fa-user-graduate"></i></i>Intern</h3>
+        </div>
+        <div class="card-body bg-secondary">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.id}</li>
+                <li class="list-group-item">Email: <a
+                href="mailto:${intern.email}">${intern.email}</a></li>
+                <li class="list-group-item">School: ${intern.school}</li>
+            </ul>
+         </div>
+    </div>
+ `
+};
+
+module.exports = generateTeamPage;
